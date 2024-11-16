2204c187e46977d2efbfd9a468854c2a5947adfd
Md Wahiduzzaman Bappy <wahidbappy253@gmail.com>
2024-11-17 00:28:51 +0600

    new design added

--- lib/main.dart
+++ lib/main.dart
@@ -28,152 +28,161 @@ class _FirstScreenState extends State<FirstScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 254, 247, 255),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 100,
-                width: 300,
-                child: Text(
-                  "Create an acount",
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: TextField(
-              keyboardType: TextInputType.text,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Row(
-            children: [
-              Checkbox(
-                checkColor: Colors.white,
-                activeColor: Colors.green,
-                value: isChecked,
-                onChanged: (bool? value) {
-                  setState(() {
-                    isChecked = value!;
-                  });
-                },
-              ),
-              const Text(
-                'I agree to the terms & conditions.',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              )
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Center(
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  backgroundColor: Colors.green,
-                  //maximumSize: Size.fromRadius(10),
-                ),
-                onPressed: () {},
-                child: const Text('Sign Up'),
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          const Center(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Or'),
-              ],
-            ),
-          ),
-          Row(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              IconButton(
-                autofocus: true,
-                focusColor: Colors.green,
-                highlightColor: Colors.grey,
-                onPressed: () {},
-                icon: Image.network(
-                  "https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png",
-                  height: 50,
-                  width: 80,
+              const SizedBox(
+                height: 100,
+              ),
+              const Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 100,
+                    width: 300,
+                    child: Text(
+                      "Create an account",
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: TextField(
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                  ),
                 ),
               ),
-              IconButton(
-                autofocus: true,
-                focusColor: Colors.green,
-                highlightColor: Colors.grey,
-                onPressed: () {},
-                icon: Image.network(
-                  "https://img.freepik.com/premium-vector/vector-blue-social"
-                  "-media-logo_1080184-225.jpg?semt=ais_hybrid",
-                  height: 50,
-                  width: 80,
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                  ),
                 ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  Checkbox(
+                    checkColor: Colors.white,
+                    activeColor: Colors.green,
+                    value: isChecked,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        isChecked = value!;
+                      });
+                    },
+                  ),
+                  const Text(
+                    'I agree to the terms & conditions.',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      backgroundColor: Colors.green,
+                      //maximumSize: Size.fromRadius(10),
+                    ),
+                    onPressed: () {},
+                    child: const Text('Sign Up'),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              const Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Or'),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    autofocus: true,
+                    focusColor: Colors.green,
+                    highlightColor: Colors.grey,
+                    onPressed: () {},
+                    icon: Image.network(
+                      "https://cdn1.iconfinder.com/data/icons/google-s-"
+                      "logo/150/Google_Icons-09-512.png",
+                      height: 50,
+                      width: 80,
+                    ),
+                  ),
+                  IconButton(
+                    autofocus: true,
+                    focusColor: Colors.green,
+                    highlightColor: Colors.grey,
+                    onPressed: () {},
+                    icon: Image.network(
+                      "https://img.freepik.com/premium-vector/vector-blue-social"
+                      "-media-logo_1080184-225.jpg?semt=ais_hybrid",
+                      height: 50,
+                      width: 80,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
-        ],
+        ),
       ),
       /*bottomNavigationBar: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
